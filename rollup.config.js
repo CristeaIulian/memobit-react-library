198f98269033becb6f8a71283f8113d693d88a26
Iulian <iulianx@gmail.com>
2025-10-27 00:20:07 +0200

    feat(theming): use fully from library

--- rollup.config.js
+++ rollup.config.js
@@ -35,6 +35,7 @@ export default {
         }),
         copy({
             targets: [
+                { src: 'src/styles/variables.scss', dest: 'dist/styles' },
                 { src: 'src/styles/theming/light-blue.scss', dest: 'dist/styles/theming' },
                 { src: 'src/styles/theming/luna.scss', dest: 'dist/styles/theming' },
                 { src: 'src/styles/theming/mintone.scss', dest: 'dist/styles/theming' },
