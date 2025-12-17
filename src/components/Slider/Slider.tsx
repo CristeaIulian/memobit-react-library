92b0b35ad1206b4a7010d8029b22e72dbc543214
Iulian <iulianx@gmail.com>
2025-12-17 18:28:46 +0200

    Extract getScoreClass from Slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -10,24 +10,19 @@ interface SliderProps {
     value: number;
     onChange: (value: number) => void;
     showValues?: boolean;
+    largeValueState?: 'danger' | 'warning' | 'info' | 'success';
 }
 
-export const Slider: FC<SliderProps> = ({ disabled = false, min = 0, max = 100, step = 1, value, onChange, showValues = true }: SliderProps): ReactElement => {
-    const getScoreClass = (score: number, minValue: number, maxValue: number): string => {
-        const range = maxValue - minValue;
-        const normalizedScore = ((score - minValue) / range) * 100;
-
-        if (normalizedScore <= 33) {
-            return 'low';
-        }
-
-        if (normalizedScore <= 66) {
-            return 'medium';
-        }
-
-        return 'high';
-    };
-
+export const Slider: FC<SliderProps> = ({
+    disabled = false,
+    largeValueState = 'info',
+    min = 0,
+    max = 100,
+    step = 1,
+    value,
+    onChange,
+    showValues = true,
+}: SliderProps): ReactElement => {
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         onChange(parseInt(e.target.value));
     };
@@ -37,7 +32,7 @@ export const Slider: FC<SliderProps> = ({ disabled = false, min = 0, max = 100, 
             <input type="range" min={min} max={max} value={value} step={step} onChange={handleChange} className="slider-input" disabled={disabled} />
             {showValues && (
                 <div className="slider-display">
-                    <span className={`slider-value slider-value-${getScoreClass(value, min, max)}`}>{value}</span>
+                    <span className={`slider-value slider-state-${largeValueState}`}>{value}</span>
                 </div>
             )}
         </div>
