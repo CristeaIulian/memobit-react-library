1ad6ccca17ce36a75ef7211252116a9f26310d3b
Iulian <iulianx@gmail.com>
2026-04-15 17:41:30 +0300

    themes: fixed themes export

--- vite.config.ts
+++ vite.config.ts
@@ -5,64 +5,68 @@ import dts from 'vite-plugin-dts';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      tsconfigPath: './tsconfig.json',
-      rollupTypes: true,
-    }),
-    viteStaticCopy({
-      targets: [
-        { src: 'src/styles/variables.scss', dest: 'styles' },
-        { src: 'src/styles/effects.scss', dest: 'styles' },
-        { src: 'src/styles/highlight.scss', dest: 'styles' },
-        { src: 'src/styles/utilities.scss', dest: 'styles' },
-        { src: 'src/styles/theming/light-blue.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/luna.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/mintone.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/dashdarkx.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/tailwind-vue-dark.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/azure-night.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/ocean-depths.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/twilight-pulse.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/cyber-forest.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/ember-night.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/arctic-blue.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/ocean-breeze.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/lavender-mist.scss', dest: 'styles/theming' },
-        { src: 'src/styles/theming/mint-meadow.scss', dest: 'styles/theming' },
-      ],
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-      },
-    },
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'cjs'],
-      fileName: (format) => format === 'es' ? 'index.esm.js' : 'index.js',
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+    plugins: [
+        react(),
+        dts({
+            tsconfigPath: './tsconfig.json',
+            rollupTypes: true,
+        }),
+        viteStaticCopy({
+            targets: [
+                { src: 'src/styles/variables.scss', dest: 'styles' },
+                { src: 'src/styles/effects.scss', dest: 'styles' },
+                { src: 'src/styles/highlight.scss', dest: 'styles' },
+                { src: 'src/styles/utilities.scss', dest: 'styles' },
+                { src: 'src/styles/theming/arctic-blue.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/azure-night.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/crimson-dusk.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/cyber-forest.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/dashdarkx.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/ember-night.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/frost-petal.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/lavender-mist.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/light-blue.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/luna.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/mint-meadow.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/mintone.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/neon-tokyo.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/ocean-breeze.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/ocean-depths.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/sandstone.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/tailwind-vue-dark.scss', dest: 'styles/theming' },
+                { src: 'src/styles/theming/twilight-pulse.scss', dest: 'styles/theming' },
+            ],
+        }),
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            },
         },
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.names?.[0]?.endsWith('.css')) {
-            return 'index.css';
-          }
-          return assetInfo.names?.[0] ?? '[name][extname]';
-        },
-      },
     },
-    sourcemap: true,
-    minify: 'terser',
-  },
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            formats: ['es', 'cjs'],
+            fileName: format => (format === 'es' ? 'index.esm.js' : 'index.js'),
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'react/jsx-runtime'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+                assetFileNames: assetInfo => {
+                    if (assetInfo.names?.[0]?.endsWith('.css')) {
+                        return 'index.css';
+                    }
+                    return assetInfo.names?.[0] ?? '[name][extname]';
+                },
+            },
+        },
+        sourcemap: true,
+        minify: 'terser',
+    },
 });
