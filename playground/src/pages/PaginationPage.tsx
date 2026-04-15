3576936d9f579fc7834cd457b82b69fa3eb771be
Iulian <iulianx@gmail.com>
2026-04-15 13:36:21 +0300

    pagination: improve desktop view

--- playground/src/pages/PaginationPage.tsx
+++ playground/src/pages/PaginationPage.tsx
@@ -3,6 +3,16 @@ import React from 'react';
 import { Pagination } from '../../../src';
 
 export const PaginationPage: React.FC = () => {
+    const [page, setPage] = React.useState<number>(1);
+
+    const pageChanged = (newPage: number) => {
+        setPage(newPage);
+    };
+
+    const onPageSizeChange = (newItemsPerPage: any) => {
+        console.log('onPageSizeChange', newItemsPerPage);
+    };
+
     return (
         <div className="pagination-page">
             <h1>Pagination Component</h1>
@@ -12,7 +22,112 @@ export const PaginationPage: React.FC = () => {
                 <h2>Basic Pagination</h2>
                 <div className="showcase-group">
                     <div className="component-group">
-                        <Pagination currentPage={3} totalPages={4} onPageChange={() => console.log('page changed')} />
+                        <Pagination currentPage={page} totalPages={24} onPageChange={pageChanged} />
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <h2>Aligned Left Pagination</h2>
+                <div className="showcase-group">
+                    <div className="component-group">
+                        <Pagination horizontalAlign="left" currentPage={page} totalPages={4} onPageChange={pageChanged} />
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <h2>Aligned Left with Page Selector</h2>
+                <div className="showcase-group">
+                    <div className="component-group">
+                        <Pagination
+                            horizontalAlign="left"
+                            currentPage={page}
+                            totalPages={4}
+                            onPageChange={pageChanged}
+                            pageSize={5}
+                            pageSizeOptions={[5, 10, 20, 50, 100]}
+                            onPageSizeChange={onPageSizeChange}
+                        />
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <h2>Aligned Center Pagination</h2>
+                <div className="showcase-group">
+                    <div className="component-group">
+                        <Pagination horizontalAlign="center" currentPage={page} totalPages={4} onPageChange={pageChanged} />
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <h2>Aligned Center with Page selector</h2>
+                <div className="showcase-group">
+                    <div className="component-group">
+                        <Pagination
+                            horizontalAlign="center"
+                            currentPage={page}
+                            totalPages={4}
+                            onPageChange={pageChanged}
+                            pageSize={5}
+                            pageSizeOptions={[5, 10, 20, 50, 100]}
+                            onPageSizeChange={onPageSizeChange}
+                        />
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <h2>Aligned Right Pagination</h2>
+                <div className="showcase-group">
+                    <div className="component-group">
+                        <Pagination horizontalAlign="right" currentPage={page} totalPages={4} onPageChange={pageChanged} />
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <h2>Aligned Right with Page selector</h2>
+                <div className="showcase-group">
+                    <div className="component-group">
+                        <Pagination
+                            horizontalAlign="right"
+                            currentPage={page}
+                            totalPages={4}
+                            onPageChange={pageChanged}
+                            pageSize={5}
+                            pageSizeOptions={[5, 10, 20, 50, 100]}
+                            onPageSizeChange={onPageSizeChange}
+                        />
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <h2>Show Page number</h2>
+                <div className="showcase-group">
+                    <div className="component-group">
+                        <Pagination horizontalAlign="left" currentPage={page} totalPages={4} onPageChange={pageChanged} showPagesNumbers />
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <h2>Show Page number with Page selector</h2>
+                <div className="showcase-group">
+                    <div className="component-group">
+                        <Pagination
+                            horizontalAlign="left"
+                            currentPage={page}
+                            totalPages={4}
+                            onPageChange={pageChanged}
+                            showPagesNumbers
+                            pageSize={5}
+                            pageSizeOptions={[5, 10, 20, 50, 100]}
+                            onPageSizeChange={onPageSizeChange}
+                        />
                     </div>
                 </div>
             </section>
