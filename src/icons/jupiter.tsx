4f718009a0d52397c0209a826486cf2ad4594450
Iulian <iulianx@gmail.com>
2026-05-11 14:48:11 +0300

    icons added

---
+++ src/icons/jupiter.tsx
@@ -0,0 +1,36 @@
+export const jupiter = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129.103 129.103" width="1em" height="1em">
+        <defs>
+            <clipPath id="jupiter-clip">
+                <circle cx="64.551" cy="64.551" r="64.551" transform="translate(331.399 204.494)" fill="none" />
+            </clipPath>
+        </defs>
+        <g transform="translate(-331.399 -204.494)">
+            <g>
+                <circle cx="64.551" cy="64.551" r="64.551" transform="translate(331.399 204.494)" fill="#f4d0b1" />
+                <g>
+                    <g clipPath="url(#jupiter-clip)">
+                        <path d="M330.159,214.5A1021.437,1021.437,0,0,1,540.3,228.515" fill="none" stroke="#dcb98e" strokeMiterlimit="10" strokeWidth="6.236" opacity="0.5" />
+                        <path d="M323.839,320.642a1021.438,1021.438,0,0,1,210.146,14.014" fill="none" stroke="#dcb98e" strokeMiterlimit="10" strokeWidth="6.236" opacity="0.5" />
+                        <path d="M306.743,333.544a1021.431,1021.431,0,0,1,210.51,6.564" fill="none" stroke="#da5423" strokeMiterlimit="10" strokeWidth="10.834" />
+                        <path d="M313.462,286.841l31.253,3.735a420.454,420.454,0,0,0,99.566,0l31.254-3.734" fill="none" stroke="#ef8f4d" strokeMiterlimit="10" strokeWidth="9.931" />
+                        <path d="M475.535,248.952l-31.254-3.734a420.4,420.4,0,0,0-99.566,0l-31.253,3.734" fill="none" stroke="#dcb98e" strokeMiterlimit="10" strokeWidth="9.931" opacity="0.3" />
+                        <path d="M475.535,260.237,444.281,256.5a420.508,420.508,0,0,0-99.566,0l-31.253,3.734" fill="none" stroke="#dcb98e" strokeMiterlimit="10" strokeWidth="3.611" opacity="0.3" />
+                        <path d="M475.535,239.37l-31.254-3.734a420.4,420.4,0,0,0-99.566,0l-31.253,3.734" fill="none" stroke="#ef8f4d" strokeMiterlimit="10" strokeWidth="1.806" />
+                        <path d="M475.535,222.818l-31.254-3.734a420.4,420.4,0,0,0-99.566,0l-31.253,3.734" fill="none" stroke="#ef8f4d" strokeMiterlimit="10" strokeWidth="6.32" />
+                        <path d="M313.462,276.043l31.253,3.734a420.4,420.4,0,0,0,99.566,0l31.254-3.734" fill="none" stroke="#ef8f4d" strokeMiterlimit="10" strokeWidth="1.806" />
+                    </g>
+                </g>
+                <circle cx="4.057" cy="4.057" r="4.057" transform="translate(370.928 274.67)" fill="#312449" />
+                <circle cx="4.057" cy="4.057" r="4.057" transform="translate(412.858 274.67)" fill="#312449" />
+                <circle cx="0.748" cy="0.748" r="0.748" transform="translate(371.961 276.912)" fill="#ffffff" />
+                <circle cx="0.748" cy="0.748" r="0.748" transform="translate(413.891 276.912)" fill="#ffffff" />
+                <path d="M395.951,333.6A64.549,64.549,0,0,0,439.23,221.154a64.31,64.31,0,0,1,16.661,43.28c0,35.65-33.6,68.495-69.248,68.495" opacity="0.2" />
+                <path d="M460.5,269.045" fill="none" />
+                <path d="M331.4,269.045" fill="none" />
+                <ellipse cx="6.342" cy="13.184" rx="6.342" ry="13.184" transform="matrix(0.059, -0.998, 0.998, 0.059, 352.542, 303.227)" fill="#ef8f4d" stroke="#f3cda6" strokeMiterlimit="10" strokeWidth="2.349" />
+            </g>
+            <path d="M412.23,308.353s-4.469,2.446-16.279,2.446c-12.729,0-16.28-2.446-16.28-2.446" fill="none" stroke="#312449" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2.709" />
+        </g>
+    </svg>
+);
---
+++ src/icons/uranus.tsx
@@ -0,0 +1,18 @@
+export const uranus = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 74.034 74.034" width="1em" height="1em">
+        <g transform="translate(-358.934 -426.342)">
+            <g>
+                <circle cx="37.017" cy="37.017" r="37.017" transform="translate(358.934 426.342)" fill="#bce1d6" />
+                <path d="M395.926,500.376A36.9,36.9,0,0,0,420.665,436.1a36.764,36.764,0,0,1,9.523,24.739c0,20.379-19.2,39.153-39.582,39.153" opacity="0.12" />
+                <path d="M395.832,426.5a36.9,36.9,0,0,0-24.74,64.273,36.762,36.762,0,0,1-9.523-24.739c0-20.378,19.2-39.153,39.583-39.153" fill="#f0f5f9" opacity="0.2" />
+            </g>
+            <g>
+                <line x2="5.344" y2="5.344" transform="translate(383.04 469.519)" fill="none" stroke="#312449" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="1.377" />
+                <line x1="5.344" y2="5.344" transform="translate(383.04 469.519)" fill="none" stroke="#312449" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="1.377" />
+                <line x2="5.344" y2="5.344" transform="translate(403.516 469.519)" fill="none" stroke="#312449" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="1.377" />
+                <line x1="5.344" y2="5.344" transform="translate(403.516 469.519)" fill="none" stroke="#312449" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="1.377" />
+            </g>
+            <circle cx="3.242" cy="3.242" r="3.242" transform="translate(392.708 483.14)" fill="#312449" />
+        </g>
+    </svg>
+);
